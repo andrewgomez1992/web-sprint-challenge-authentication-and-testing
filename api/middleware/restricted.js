85db14ffefd28cb4588b7322f283c30145293c4b
Andrew Gomez <drewgomez209@gmail.com>
2022-06-29 07:43:40 -0700

    login working

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,31 +1,18 @@
-const jwt = require('jsonwebtoken')
-require('dotenv')
-
-const secret = process.env.SECRET || 'ssh'
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets/secret");
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization
-  if (!token) {
-    res.status(401).json({ message: 'token required' })
+  const token = req.headers.authorization;
+  if (token === undefined) {
+    res.status(403).json({ message: "token required" });
   } else {
-    jwt.verify(token, secret, (err, decodedToken) => {
+    jwt.verify(token, JWT_SECRET, async (err, decodedToken) => {
       if (err) {
-        res.status(401).json({ message: 'token invalid' })
+        next({ status: 401, message: "token invalid" });
       } else {
-        req.decodedToken = decodedToken
-        next()
+        req.decodedToken = decodedToken;
+        next();
       }
-    })
+    });
   }
-  /*
-    IMPLEMENT
-
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
 };
--- api/server.test.js
+++ api/server.test.js
@@ -8,32 +8,32 @@ const db = require('../data/dbConfig')
 
 const userA = { username: 'foo', password: 'bar' }
 
-// beforeAll(async () => { // runs before everything
-//   await db.migrate.rollback()
-//   await db.migrate.latest()
-// })
+beforeAll(async () => { // runs before everything
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
 
-// afterAll(async () => {
-//   await db.destroy() // only use destroy when doing tests
-// })
+afterAll(async () => {
+  await db.destroy() // only use destroy when doing tests
+})
 
 test('sanity', () => {
   expect(true).not.toBe(false)
 })
 
-// describe('server', () => {
-//   describe('[POST] /api/auth/register', () => {
-//     beforeEach(async () => {
-//       await db('users').truncate()
-//     })
-//     it('adds a new user with bcrypted password on success', async () => {
-//       await request(server).post('/api/auth/register').send(userA)
-//       const user = await db('users').first()
-//       expect(user).toHaveProperty('id')
-//       expect(user).toHaveProperty('username')
-//       expect(user).toHaveProperty('password')
-//     })
-//   })
-// })
+describe('server', () => {
+  describe('[POST] /api/auth/register', () => {
+    beforeEach(async () => {
+      await db('users').truncate()
+    })
+    it('adds a new user with bcrypted password on success', async () => {
+      await request(server).post('/api/auth/register').send(userA)
+      const user = await db('users').first()
+      expect(user).toHaveProperty('id')
+      expect(user).toHaveProperty('username')
+      expect(user).toHaveProperty('password')
+    })
+  })
+})
 
 
