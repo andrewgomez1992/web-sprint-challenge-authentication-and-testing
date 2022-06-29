70bc0d9571a117489e1b8de0286516c31e640d6b
Andrew Gomez <drewgomez209@gmail.com>
2022-06-29 10:18:01 -0700

    re-added instructions for login

--- api/server.test.js
+++ api/server.test.js
@@ -4,34 +4,34 @@ const bcrypt = require('bcryptjs')
 const token = require('../api/auth/token')
 const jwt = require('jsonwebtoken')
 
-const db = require('../data/dbConfig')
+const db = require('../data/dbConfig');
 
-const userA = { username: 'foo', password: 'bar' }
+const userA = { username: 'foo', password: 'bar' };
 
 beforeAll(async () => { // runs before everything
-  await db.migrate.rollback()
-  await db.migrate.latest()
+  await db.migrate.rollback();
+  await db.migrate.latest();
 })
 
 afterAll(async () => {
-  await db.destroy() // only use destroy when doing tests
+  await db.destroy(); // only use destroy when doing tests
 })
 
 test('sanity', () => {
-  expect(true).not.toBe(false)
+  expect(true).not.toBe(false);
 })
 
 describe('server', () => {
   describe('[POST] /api/auth/register', () => {
     beforeEach(async () => {
-      await db('users').truncate()
+      await db('users').truncate();
     })
     it('adds a new user with bcrypted password on success', async () => {
-      await request(server).post('/api/auth/register').send(userA)
-      const user = await db('users').first()
-      expect(user).toHaveProperty('id')
-      expect(user).toHaveProperty('username')
-      expect(user).toHaveProperty('password')
+      await request(server).post('/api/auth/register').send(userA);
+      const user = await db('users').first();
+      expect(user).toHaveProperty('id');
+      expect(user).toHaveProperty('username');
+      expect(user).toHaveProperty('password');
     })
   })
 })
