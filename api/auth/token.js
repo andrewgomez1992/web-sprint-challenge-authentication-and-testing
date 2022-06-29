6606ce4e87eb50c0c9148d4e1a6a365ff7c6a55a
Andrew Gomez <drewgomez209@gmail.com>
2022-06-29 08:07:16 -0700

    register working, login giving error

--- api/auth/token.js
+++ api/auth/token.js
@@ -1,6 +1,5 @@
 require('dotenv')
 const jwt = require('jsonwebtoken')
-const secret = process.env.SECRET || 'nobodys gonna know'
 
 const generateToken = (user) => {
     const { id: subject, name } = user;
