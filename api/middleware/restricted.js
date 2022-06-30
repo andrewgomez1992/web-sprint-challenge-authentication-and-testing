09c72a1d1e927f06ee6d19e7fa9de775428b0fcf
Andrew Gomez <drewgomez209@gmail.com>
2022-06-29 20:16:35 -0700

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -3,15 +3,16 @@ const secret = require('../secrets/secret');
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
-  if (token) {
-    jwt.verify(token, secret, async (err, decoded) => {
-      if (err != null) {
-        res.status(401).json({ message: 'token invalid' })
-        return
-      }
-      next()
-    })
-  } else {
+  if (token === undefined) {
     res.status(401).json({ message: 'token required' })
+  } else {
+    jwt.verify(token, secret, async (err, decodedToken) => {
+      if (err) {
+        next({ status: 401, message: 'token invalid' })
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
   }
 };
